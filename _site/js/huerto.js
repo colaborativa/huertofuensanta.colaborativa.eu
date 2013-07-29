12f1daa97e3525aa3bf20675c7637f8833709240
magda <magdasanchez@colaborativa.eu>
2013-07-29 17:33:14 +0200

    Añado comentarios a huerto.js

--- _site/js/huerto.js
+++ _site/js/huerto.js
@@ -1,19 +1,21 @@
 /* 
-	http://www.flickr.com/services/api/explore/flickr.groups.pools.getPhotos
+  Descripción: 
+    A través del siguiente código se obtienen las imágenes insertadas dentro del grupo de Flickr Huerto Fuensanta 
+    (http://www.flickr.com/groups/2233980@N22/). Además de la imagen se extraen otros datos de utilidad como son:
+      - Fecha en la que se tomó la foto
+      - Propietario (nombre, icono)
+      - Descripción
+    Las fotos se ordenan por la fecha en las que fueron tomadas.
+
+	Links de referencia:
+  http://www.flickr.com/services/api/explore/flickr.groups.pools.getPhotos
 	http://www.flickr.com/services/api/flickr.groups.pools.getPhotos.html
 
-	group_id: 2233980@N22
-    "id": "8655193988"
-    "secret": "07b236521f"
-    "server": "8113"
-    "farm": 9
-    "iconserver": "8251", "iconfarm": 9
-    http://farm9.staticflickr.com/8251/buddyicons/50381188@N06.jpg
-    http://farm9.staticflickr.com/8251/50381188@N06.jpg
-    http://farm9.staticflickr.com/8113/8655193988_07b236521f_b.jpg
-	"http://farm" + item.farm + ".staticflickr.com/" + item.server + "/" + item.id + "_" + item.secret + "_b.jpg"
-	JSON OUTPUT:
-{ "photos": { "page": 1, "pages": 1, "perpage": 100, "total": 58, 
+	Para iconos usar:  http://farm9.staticflickr.com/8251/buddyicons/50381188@N06.jpg
+  Para fotos de un grupo usar: http://farm9.staticflickr.com/8113/8655193988_07b236521f_b.jpg
+	
+  Salida JSON:
+  { "photos": { "page": 1, "pages": 1, "perpage": 100, "total": 58, 
     "photo": [
       { "id": "8677001289", "owner": "50381188@N06", "secret": "f0354915da", "server": "8254", 
       	"farm": 9, "title": "Huerto social y ecológico de la Fuensanta Córdoba 22-04-2012", 
@@ -24,21 +26,17 @@
       "ispublic": 1, "isfriend": 0, "isfamily": 0, "ownername": "colaborativa.eu", 
       "dateadded": "1374751234" },
       ....
-] }, "stat": "ok" }
+  ] }, "stat": "ok" }
 
+  URL:http://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key=ee4bdc6841d42f90d9ca5e598e99d2f3
+  &group_id=2233980%40N22
+  &extras=@%2C+owner_name%2C+icon_server
+  &format=json&nojsoncallback=1
+  &api_sig=9c8bb3ae7ac1e2a593358e9eaadf42c3
 
-	URL: http://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&
-	api_key=eb8a57b5872cf567fbbb1a42bdbd26ad&group_id=2233980%40N22&format=json&
-	nojsoncallback=1&auth_token=72157634788201621-216bf626aae13e55&api_sig=c5a2baef5adcbd9df5716fd46265becd
-
-http://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key=ee4bdc6841d42f90d9ca5e598e99d2f3
-&group_id=2233980%40N22
-&extras=@%2C+owner_name%2C+icon_server
-&format=json&nojsoncallback=1
-&api_sig=9c8bb3ae7ac1e2a593358e9eaadf42c3
-
-<div class="carousel-inner">
-<div class="item">
+  Estructura HTML:
+  <div class="carousel-inner">
+  <div class="item">
           <img src="http://farm9.staticflickr.com/8113/8655193988_07b236521f_b.jpg" alt="">
           <div class="container">
             <div class="carousel-caption">
@@ -47,14 +45,16 @@ http://api.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_ke
             </div>
           </div>
         </div>
-....
-</div>
+  ....
+  </div>
 
-huertofuensanta
-Clave: ee4bdc6841d42f90d9ca5e598e99d2f3
-Secreto: ce66d3877f4238c3
+  Datos API de Flickr:
+  huertofuensanta
+  Clave: ee4bdc6841d42f90d9ca5e598e99d2f3
+  Secreto: ce66d3877f4238c3
 
 */
+
 Date.locale = {
     es: {
        month_names: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
@@ -67,7 +67,6 @@ url += '&group_id=2233980%40N22';
 url += '&extras=description%2Cdate_taken%2C+owner_name%2C+icon_server';
 url += '&per_page=500'; // by default 50
 url += '&format=json&nojsoncallback=1';
-//url += '&api_sig=9c8bb3ae7ac1e2a593358e9eaadf42c3';
 // Assign handlers immediately after making the request,
 // and remember the jqxhr object for this request
 var jqxhr = $.getJSON( url, function() {
