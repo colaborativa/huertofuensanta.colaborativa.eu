c45dc5c995ebe62a485ed955d6552de8b3c9d027
magda <magdasanchez@colaborativa.eu>
2013-08-19 18:29:14 +0200

    Trying google API for adding a member to a group

--- js/google-groups.js
+++ js/google-groups.js
@@ -1,9 +1,18 @@
 /*
+
+	Using Java Script client Google
 	- Authorization Code in Javascript and html
 	https://code.google.com/p/google-api-javascript-client/source/browse/samples/authSample.html
 
 	- Consola de manego de APPS en Google (creación clientID for OAuth2.0)
 	https://code.google.com/apis/console/#project:425984504337
+	https://developers.google.com/api-client-library/javascript/dev/dev_jscript
+
+	- Important link for using with gapi
+	https://developers.google.com/admin-sdk/groups-settings/v1/reference/groups/update
+	https://developers.google.com/admin-sdk/directory/v1/reference/members/insert (**)
+
+	- Goolgle Apis Explorer: https://developers.google.com/api-client-library/javascript/dev/dev_jscript
 
 	- Pending: Get Secret Info from JSON
 */
@@ -22,7 +31,8 @@
   var groups_apiKey = 'AIzaSyBZpkN4-NjFMyzMoL6ow-24Vz4haQHckiI';
 
   // https://developers.google.com/admin-sdk/groups-settings/auth
-  var scopes = 'https://www.googleapis.com/auth/apps.groups.settings';
+  var scopes = 'https://www.googleapis.com/auth/admin.directory.group';
+  //
 
   // Use a button to handle authentication the first time.
   function handleClientLoad() {
@@ -37,12 +47,13 @@
 
 
   function handleAuthResult(authResult) {
-  	//console.log("authResult: "+authResult);
     var authorizeButton = document.getElementById('authorize-button');
     if (authResult && !authResult.error) {
       authorizeButton.style.visibility = 'hidden';
       makeApiCall();
       console.log("OK!!!");
+      var authTimeout = (authResult.expires_in - 5 * 60) *1000; // every 5 minutes miliseconds
+      setTimeout(checkAuth, authTimeout); // Refresh a token so it won´t 
     } else {
       authorizeButton.style.visibility = '';
       authorizeButton.onclick = handleAuthClick;
@@ -56,7 +67,24 @@
   }
 
   // Load the API and make an API call.  Display the results on the screen.
-  function makeApiCall() {
+function makeApiCall() {
+	gapi.client.load('plus', 'v1', function() {
+		  var request = gapi.client.request({
+		  	'path':'admin/directory/v1/groups',
+		  	'params': {'groupKey':'huertofuensanta@googlegroups.com'},
+		  	'method': 'POST',
+		  	'headers':{'Content-type':'application/json'},
+		  	'body': JSON.stringify({'email':'magda@cortaypega.org',"role": "MEMBER"}),
+		  	'callback': function(jsonResponse, rawResponse){
+		  		// jsonResponse = false is response is not valid json
+		  		// rawResponse = full http response as a string
+		  		// status in raw response
+		  		console.log(jsonResponse);
+		  		}
+		  	}); // end gapi request
+			//request.execute(
+   }); // end gapi.client.load*/
+} // end function makeApiCall
     /*gapi.client.load('plus', 'v1', function() {
       var request = gapi.client.plus.people.get({
         'userId': 'me'
@@ -70,7 +98,7 @@
 
         document.getElementById('content').appendChild(heading);
       });
-    });*/
+    });
 	
 	console.log("OK!!!");
-  }
+  }*/
