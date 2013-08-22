d9e645f8672320271be7223987fb06b99fb12f73
magda <magdasanchez@colaborativa.eu>
2013-08-22 13:42:35 +0200

    preloading next and previous images of carrousel on Slide Event

--- js/huerto.js
+++ js/huerto.js
@@ -260,6 +260,49 @@ function ActivitiesAdd(features){
                 $('.actividad').popover();
          });
 }
+/**
+ *
+ *
+ *  MYCAROUSEL FUNCTIONS
+ *
+ *
+**/
+$('#myCarousel').on("slid", function(e) {
+    console.log("slide");
+    //SCROLLING LEFT
+    var prevItem = $('.active.item', this).prev('.item');// Get the right item
+
+    //Account for looping to LAST image
+    if(!prevItem.length){
+        prevItem = $('.active.item', this).siblings(":last");
+    }
+
+    //Get image selector
+    prevImage = prevItem.find('img');
+
+    //Remove class to not load again - probably unnecessary
+    if(prevImage[1].getAttribute('class') == 'lazy-load'){
+        prevImage.removeClass('lazy-load');
+        prevImage[1].src = prevImage[1].getAttribute('lazy-load-src');
+    }
+
+    //SCROLLING RIGHT
+    var nextItem = $('.active.item', this).next('.item');
+
+    //Account for looping to FIRST image
+    if(!nextItem.length){
+        nextItem = $('.active.item', this).siblings(":first");
+    }
+
+    //Get image selector
+    nextImage = nextItem.find('img');
+
+    //Remove class to not load again - probably unnecessary
+    if(nextImage[1].getAttribute('class') == 'lazy-load'){
+        nextImage.removeClass('lazy-load');
+        nextImage[1].src = nextImage[1].getAttribute('lazy-load-src');
+    }
+});
 /*
 
     GOOGLE CALENDAR PUBLIC
